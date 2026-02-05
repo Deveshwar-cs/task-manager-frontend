39bc0e876344b0845a544a26e981f68f8f973692
Deveshwar-cs <goboby744@gmail.com>
2026-02-05 17:57:58 +0530

    add render url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // baseURL: "http://localhost:5001/api",
+  baseURL: "https://task-manager-mern-hu3h.onrender.com/api",
   headers: {
     content: "application/json",
   },
